c456c5fb665ce714af84bbc86e520fa0519ffab0
nickolasram <nickolasram@gmail.com>
2024-08-06 23:29:16 -0700

    initial banner styling

---
+++ Models/Project.tsx
@@ -0,0 +1,8 @@
+export interface Project{
+    title: string;
+    displayImage?: string;
+    github?: string;
+    site?: string;
+    description?: string;
+    role: string; 
+}
--- app/page.tsx
+++ app/page.tsx
@@ -13,11 +13,13 @@ export default function Home() {
 
   function parallax() {
     var s = document.getElementById("banner-div");
+    var r = document.getElementById("banner-wrapper-backdrop-filter");
     var yPos = 0 - window.pageYOffset/4;  
     s!.style.top = yPos + "%"; 
     var z = document.getElementById("carousel-wrapper");
     var filterValue = Math.min(0.01 * Math.max(10, window.pageYOffset/4), 1)
     z!.style.filter = `brightness(${filterValue})`;
+    r!.style.filter = `brightness(${filterValue})`;
     setPageYOffset(window.pageYOffset)
   }
 
@@ -33,19 +35,37 @@ export default function Home() {
         <p>udqwi</p>
       </Stack> */}
       <div className="banner-wrapper">
+        <div className="banner-wrapper-backdrop-filter" id="banner-wrapper-backdrop-filter"></div>
         <div className="banner-div" id="banner-div">
-          <h1 className="banner-heading">
-            This is a welcoming
-          </h1>
-          <Image
-            src="https://imgur.com/voEgSSD.jpeg"
-            height={100}
-            width={100}
-            alt='personal photo'
-          />
-          <p className="banner-p">
-            This is a paragraph containing details.
-          </p>
+          <div className="banner-grid">
+            <Image
+              src="https://imgur.com/voEgSSD.jpeg"
+              height={300}
+              width={300}
+              alt='personal photo'
+              className="banner-image"
+              style={{float: 'right',
+                      margin: 5}}
+            />
+            <div className="banner-headings-flex">
+              <h1 className="banner-heading">
+                Nickolas Ramirez
+              </h1>
+              <h2 className="banner-subheading">
+                Web Developer - UI/UX Engineer
+              </h2>
+            </div>
+            <p className="banner-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Cras ornare dui erat, et hendrerit augue consectetur et. 
+            Nullam eleifend dapibus elit nec condimentum. 
+            Aliquam posuere maximus libero, eu ultricies diam. 
+            Nulla efficitur tempor velit sit amet sagittis. 
+            Vivamus quis leo eget enim pretium tincidunt ut sed tortor. 
+            Proin a euismod elit. Maecenas convallis at tortor quis sollicitudin. 
+            Aliquam tincidunt malesuada dignissim. 
+            Ut viverra augue mauris, a euismod mauris efficitur non.</p>
+          </div>
         </div>
         <div className="links-div">
           <p>Here will be links</p>
@@ -61,16 +81,27 @@ export default function Home() {
         <div className="carousel-item">
         </div>
       </div>
+      <div className="carousel-wrapper">
+      </div>
       <style jsx>{`
           .banner-wrapper{
             height: 75vh;
-            background-color: red;
+            background-color: gray;
             width: 100vw;
             z-index: 1;
             overflow: visible;
             position: relative;
+            backdrop-filter: brightness(0.1);
+            display: grid;
+            grid-template-areas: "q";
+          }
+          .banner-wrapper-backdrop-filter{
+            background-color: gray;
+            grid-area: "q";
+            filter: brightness(0.1);
           }
           .banner-div{
+            grid-area: "q";
             height: 100vh;
             width: 100vw;
             background-color: blue;
@@ -78,6 +109,34 @@ export default function Home() {
             z-index: 2;
             position: absolute;
             top: 0;
+            display: flex;
+            align-items: flex-start;
+            justify-content: center;
+          }
+          .banner-grid{
+            margin-top: 2rem;
+            height: fit-content;
+            width: 60%;
+          }
+          .banner-headings-flex{
+            height: 250px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .banner-heading {
+            font-size: 3rem;
+          }
+          .banner-subheading {
+            color: #ccc;
+          }
+          .banner-image{
+            
+          }
+          .banner-p{
+            grid-area: c;
+            
+            font-size: 1.5rem;
           }
           .links-div{
             z-index: 1;
@@ -86,7 +145,7 @@ export default function Home() {
             bottom: 0;
           }
           .carousel-wrapper{
-            background-color: green;
+            background-color: gray;
             width: 100vw;
             height: 75vh;
             z-index: 1;
@@ -98,9 +157,9 @@ export default function Home() {
           .carousel-item{
             height: 70vh;
             width: 20%;
-            background-color: grey;
+            background-color: green;
           }
-          h1, p{
+          h1, h2, p{
             color: white;
           }
       `}</style>
